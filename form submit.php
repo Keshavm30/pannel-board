f29e0c01a6303c505b78dee3ee947b5a95fb1047
Keshavm30 <59768236+Keshavm30@users.noreply.github.com>
2024-03-18 13:28:13 +0530

    Add files via upload

---
+++ form submit.php
@@ -0,0 +1,6 @@
+<?php  
+$Board=$_GET["Board"];//receiving Board field value in $Board variable  
+$Standard=$_GET["Standard"];//receiving Standard field value in $Standard variable  
+$Subject=$_GET["Subject"];//receiving Subject field value in $Subject variable  
+echo "http://localhost/pannel board/$Board/$Standard/$Subject";  
+?>  
